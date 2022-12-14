7e1e6520dfd9d53b69cf01bd0b548aa4d49226fa
cirineup <edenilsonf.silveira@gmail.com>
2022-12-14 20:25:20 -0300

    iniciando configurações e telas

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // await FirebaseAuth.instance.signInAnonymously();
+  runApp(const MyApp());
+  }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Chat",
+      home: const ChatScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          iconTheme: const IconThemeData(
+            color: Colors.blue,
+          )),
+    );
+  }
+}
+
+  
